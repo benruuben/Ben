b1c9fbe5805febf453d592f8bf0b195e5aac03f6
Benruuben <benruuben@gmail.com>
2015-10-05 12:29:21 +0300

    Kõik taggid.

---
+++ PHP.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <title>Gang</title>
+    <base>
+
+</head>
+<body>
+<audio controls autoplay>
+    <source src="/audio/muss.mp3" type="audio/mpeg" />
+    <source src="/audio/muss.ogg" type="audio/ogg" />
+    Your browser does not support the audio tag.
+</audio>
+<video width="320" height="240" controls>
+    <source src="/video/vid.mp4" type="video/mp4" />
+    <source src="/video/vid.ogg" type="video/ogg" />
+    Your browser does not support the video tag.
+</video>
+<div class="image">
+    <h1>Hood</h1>
+    <img src="http://www.the-guard.net/uploads/thumbs/026a12c05-1.jpg" alt="Pic" style="width:304px;height:228px;"/>
+    <p>Külasta seda lehte: <br> <a href="https://en.wikipedia.org/wiki/Gang">See leht</a>.</p>
+</div>
+
+<article>
+<p>Eesti<abbr title="World Health Organization">ON</abbr> mereriik.</p>
+</article>
+<aside>
+    <h4>Epcot Center</h4>
+    <p>The Epcot Center is a theme park in Disney World, Florida.</p>
+</aside>
+<h2><b>Mingi kraam</b></h2>
+<table align="center">
+    <caption>Jeees</caption>
+    <tr>
+        <th>Mis värv:</th>
+        <th>Mis meeskond:</th>
+        <th>Mis ala:</th>
+    </tr>
+    <tr>
+        <td>Sinine</td>
+        <td>Cleveland</td>
+        <td>Korvpall</td>
+    </tr>
+</table align="center">
+<h3>Lemmik mängijad kossus:</h3>
+<ul>
+    <li>Kyrie Irving</li>
+    <li>LBJ</li>
+    <li>Iman Shumpert</li>
+    <li>Vince Carter</li>
+    <li>James Harden</li>
+</ul>
+<hr>
+<h4>Lemmik spordialad:caption</h4>
+<ol>
+    <li>Koss</li>
+    <li>Kerka</li>
+    <li>Disc</li>
+</ol>
+<ul>
+    <li>User <bdi>hrefs</bdi>: 60 points</li>
+    <li>User <bdi>jdoe</bdi>: 80 points</li>
+    <li>User <bdi>إيان</bdi>: 90 points</li>
+</ul>
+<p><bdo dir="rtl">This paragraph will go right-to-left.</bdo></p>
+<blockquote cite="http://www.worldwildlife.org/who/index.html">
+    For 50 years, WWF has been protecting the future of nature. The world's leading conservation organization, WWF works in 100 countries and is supported by 1.2 million members in the United States and close to 5 million globally.
+</blockquote>
+<button type="button" onclick="alert('Hello world!')">Click Me!</button>
+<canvas id="myCanvas">Your browser does not support the HTML5 canvas tag.</canvas>
+<cite></cite>
+<code></code>
+<col></col>
+<colgroup></colgroup>
+<datalist></datalist>
+<dd></dd>
+<del></del>
+<details></details>
+<dfn></dfn>
+<dialog></dialog>
+<dl></dl>
+<dt></dt>
+<em></em>
+<embed></embed>
+<fieldset></fieldset>
+<figcaption></figcaption>
+<figure></figure>
+<footer></footer>
+<form></form>
+<i></i>
+<iframe></iframe>
+<input></input>
+<ins></ins>
+<kbd></kbd>
+<keygen></keygen>
+<label></label>
+<legend></legend>
+<main></main>
+<map></map>
+<mark></mark>
+<menu></menu>
+<menuitem></menuitem>
+<meta></meta>
+<meter></meter>
+<nav></nav>
+<noscript></noscript>
+<object></object>
+<optgroup></optgroup>
+<option></option>
+<output></output>
+<param></param>
+<pre></pre>
+<progress></progress>
+<q></q>
+<s></s>
+<samp></samp>
+<script></script>
+<section></section>
+<select></select>
+<small></small>
+<source>
+<span></span>
+<strong></strong>
+<style></style>
+<sub></sub>
+<summary></summary>
+<sup></sup>
+<tbody></tbody>
+<textarea></textarea>
+<tfoot></tfoot>
+<thead></thead>
+<time></time>
+<var></var>
+<wbr>
+
+
+
+
+
+
+
+
+
+
+
+<!--Ja nii ongi-->
+<address>
+    Dead County
+</address>
+<p>Click on the sun:</p>
+<img src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSU2TqaexbiA44auxkdJi09A7Snbjj3CFH9Gq8YrU19hvlLDso4lg" alt="sun" usemap="#Planet">
+<map name="Planet">
+    <area shape="rect" coords="0,0,300,300" href="http://solarviews.com/raw/vss/VSS00031.jpg">
+</map>
+
+
+</body>
+</html>
